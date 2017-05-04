b0a1aaa6d59dfcbb0533cfa062e2bd9903dacaf0
翻滚的葫芦球 <yongle.lin@outlook.com>
2017-05-04 15:17:34 +0800

    feat(envs.py): parse argument

---
+++ envs/envs.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import argparse
+
+subparser_config = {
+    'new': {
+        'help': 'new package',
+        'arguments': ['package'],
+    },
+    'delete': {
+        'help': 'delete package',
+        'arguments': ['package'],
+    },
+    'install': {
+        'help': 'install package',
+        'arguments': ['package'],
+    },
+    'uninstall': {
+        'help': 'uninstall package',
+        'arguments': ['package'],
+    },
+    'sync': {
+        'help': 'sync',
+        'arguments': [],
+    },
+    'edit': {
+        'help': 'edit package',
+        'arguments': ['package'],
+    },
+    'list': {
+        'help': 'list all packages',
+        'arguments': [],
+    },
+    'info': {
+        'help': 'show package info',
+        'arguments': ['package'],
+    },
+}
+
+parser = argparse.ArgumentParser()
+subparsers = parser.add_subparsers(dest='subparser')
+subparsers.parsers = {}
+for key, value in subparser_config.items():
+    parser_key = subparsers.add_parser(key, help=value['help'])
+    for argument in value['arguments']:
+        parser_key.add_argument(argument)
+    subparsers.parsers[key] = parser_key
+args = parser.parse_args()
